0c6942bc454a0364dc9f3b01af743ae5acc6b5dd
Angshuman Bardhan <bardhanangshu@gmail.com>
2023-05-10 18:32:43 +0530

    home page

---
+++ src/App.styled.js
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+export const AppStyle = styled.div`
+min-width: 100vw;
+min-height: 100vh;
+width: 100vw;
+height: 100vh;
+display: flex;
+overflow-x: hidden;
+position: fixed;
+`;
